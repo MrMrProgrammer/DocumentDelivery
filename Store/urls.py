5a462c529aabac0d44eecf9304f9219b076a1b94
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-11-07 10:54:53 +0330

    add order app

--- Store/urls.py
+++ Store/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('save-store', views.show_store, name='show-store'),
+    path('show-store', views.show_store, name='show-store'),
 ]
