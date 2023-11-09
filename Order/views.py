e98d0068d0b4de9e7ee25edfe9871f867562502a
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-11-10 00:58:53 +0330

    try to set dropdown box

--- Order/views.py
+++ Order/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from builtins import str
+
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import OrderForm
+from .models import Order
 
 
 # Create your views here.
@@ -16,19 +19,27 @@ class GetDocument(View):
         return render(request, 'Order/get-document.html', context)
 
     def post(self, request):
-
         register_form = OrderForm(request.POST)
 
         if register_form.is_valid():
+            store = register_form.cleaned_data.get('store_name')
+            order_number = register_form.cleaned_data.get('order_number')
+            shipping_method = register_form.cleaned_data.get('shipping_method')
+            description = register_form.cleaned_data.get('description')
 
-            store = register_form.cleaned_data.get('store')
+            print(store)
+            print(order_number)
+            print(shipping_method)
+            print(description)
 
-            new_store = Store(
-                store_name=store_name,
-                is_active=True,
+            new_doc = Order(
+                store_id=store,
+                order_number=order_number,
+                shipping_method=shipping_method,
+                description=description,
             )
 
-            new_store.save()
+            new_doc.save()
 
             return redirect('show-store')
 
