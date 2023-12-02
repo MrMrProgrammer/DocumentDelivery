e8985fd03a6e6b1f28f98d751e16cae43f62035b
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-02 11:48:40 +0330

    add urls for store

--- Store/urls.py
+++ Store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('show-store', views.show_store, name='show-store'),
     path('add-store', views.StoreRegisterView.as_view(), name='add-store'),
+    path('delete-store/<int:store_id>', views.delete_store, name='delete-store'),
 ]
