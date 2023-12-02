a0a5b6e311ee10531abd6e3559f9b0e6306f22f0
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-02 11:48:51 +0330

    edit store model

--- Store/models.py
+++ Store/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Store(models.Model):
     store_name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.store_name
