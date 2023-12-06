115aa532ae0f2f7f985e4877c1d7f8ac2513df3c
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-06 23:39:34 +0330

    add update page for store

--- Store/urls.py
+++ Store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('show-store', views.show_store, name='show-store'),
     path('add-store', views.StoreRegisterView.as_view(), name='add-store'),
     path('delete-store/<int:store_id>', views.delete_store, name='delete-store'),
+    path('update-store/<int:store_id>', views.UpdateStoreView.as_view(), name='update-store'),
 ]
