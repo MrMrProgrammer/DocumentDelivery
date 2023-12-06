30a3ad2523c57e043ff5db7774387d1a5ab1c6b9
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-06 23:39:14 +0330

    add update page for store

--- Store/forms.py
+++ Store/forms.py
@@ -16,39 +16,31 @@ class StoreRegisterForm(forms.Form):
 
 class UpdateStoreForm(forms.ModelForm):
 
-    is_active = forms.BooleanField(required=False, label='فعال / غیرفعال')
-    # food_photo = forms.ImageField(required=False, widget=forms.FileInput)
-
     class Meta:
         model = Store
+        fields = ["store_name", "is_active"]
+
+    # is_active = forms.BooleanField(required=False, label='فعال / غیرفعال')
+    # # food_photo = forms.ImageField(required=False, widget=forms.FileInput)
+
+    # class Meta:
+    #     model = Store
         # fields = ['food_photo', 'food_name', 'food_price', 'food_recipe', 'is_active']
 
-        # widgets = {
-        #
-        #     'food_name': forms.TextInput(attrs={
-        #         'class': 'form-control',
-        #         'placeholder': 'نام غذا'
-        #     }),
-        #     'food_price': forms.NumberInput(attrs={
-        #         'class': 'form-control',
-        #         'placeholder': 'قیمت'
-        #     }),
-        #     'food_recipe': forms.Textarea(attrs={
-        #         'class': 'form-control',
-        #         'placeholder': 'دستورپخت'
-        #     }),
-        #
-        #     'food_photo': forms.ClearableFileInput(attrs={
-        #         'class': 'form-control',
-        #         'placeholder': 'عکس غذا',
-        #
-        #     }),
-        # }
-        #
-        # labels = {
-        #     'food_name': 'نام غذا',
-        #     'food_price': 'قیمت',
-        #     'food_recipe': 'دستورپخت',
-        #     'is_active': 'فعال / غیرفعال',
-        #     'food_photo': 'عکس غذا'
-        # }
+        widgets = {
+
+            'store_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'نام فروشگاه'
+            }),
+            #
+            # 'is_active': forms.NullBooleanSelect(attrs={
+            #     'class': 'form-control',
+            #     'placeholder': 'قیمت'
+            # }),
+        }
+
+        labels = {
+            'store_name': 'نام فروشگاه',
+            'is_active': 'فعال / غیرفعال'
+        }
