dcf095e5ee1bf9af91d0af4197e5385f024436f8
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-09 15:11:46 +0330

    change template

--- Store/forms.py
+++ Store/forms.py
@@ -14,6 +14,7 @@ class StoreRegisterForm(forms.Form):
     is_active = forms.BooleanField()
 
 
+<<<<<<< HEAD
 class UpdateStoreForm(forms.ModelForm):
 
     class Meta:
@@ -44,3 +45,43 @@ class UpdateStoreForm(forms.ModelForm):
             'store_name': 'نام فروشگاه',
             'is_active': 'فعال / غیرفعال'
         }
+=======
+# class UpdateStoreForm(forms.ModelForm):
+#
+#     is_active = forms.BooleanField(required=False, label='فعال / غیرفعال')
+#     # food_photo = forms.ImageField(required=False, widget=forms.FileInput)
+#
+#     class Meta:
+#         model = Store
+#         # fields = ['food_photo', 'food_name', 'food_price', 'food_recipe', 'is_active']
+#
+#         # widgets = {
+#         #
+#         #     'food_name': forms.TextInput(attrs={
+#         #         'class': 'form-control',
+#         #         'placeholder': 'نام غذا'
+#         #     }),
+#         #     'food_price': forms.NumberInput(attrs={
+#         #         'class': 'form-control',
+#         #         'placeholder': 'قیمت'
+#         #     }),
+#         #     'food_recipe': forms.Textarea(attrs={
+#         #         'class': 'form-control',
+#         #         'placeholder': 'دستورپخت'
+#         #     }),
+#         #
+#         #     'food_photo': forms.ClearableFileInput(attrs={
+#         #         'class': 'form-control',
+#         #         'placeholder': 'عکس غذا',
+#         #
+#         #     }),
+#         # }
+#         #
+#         # labels = {
+#         #     'food_name': 'نام غذا',
+#         #     'food_price': 'قیمت',
+#         #     'food_recipe': 'دستورپخت',
+#         #     'is_active': 'فعال / غیرفعال',
+#         #     'food_photo': 'عکس غذا'
+#         # }
+>>>>>>> 60211c6 (change template)
