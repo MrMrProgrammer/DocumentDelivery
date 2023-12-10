be1b5987df3d7648ccac2ea41ee88f3d6a4e4c0f
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-10 19:58:12 +0330

    edit

--- Order/models.py
+++ Order/models.py
@@ -1,20 +1,24 @@
 from datetime import datetime
 from django.db import models
 from django.utils import timezone
-
 from Store.models import Store
+
+
+class ShippingMethod(models.Model):
+    en_name = models.CharField(max_length=20)
+    fa_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.fa_name
+
+    class Meta:
+        verbose_name = "Shipping Method"
 
 
 class Order(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
     order_number = models.TextField(blank=True, null=True)
-    shipping_methods = (
-        ("post", "پست"),
-        ("tipax", "تیپاکس"),
-        ("delivery", "پیک موتوری"),
-    )
-    shipping_method = models.CharField(max_length=20, choices=shipping_methods, null=True, blank=True)
-    date = models.DateTimeField(null=True, blank=True)
+    shipping_method = models.ForeignKey(ShippingMethod, on_delete=models.CASCADE, null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
     time = models.TimeField(null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
     document_defects = models.CharField(max_length=200, blank=True, null=True)
