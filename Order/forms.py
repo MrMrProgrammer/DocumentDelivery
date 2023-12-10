af4de2ac02860337033709690e851ad82949ff62
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-10 19:57:34 +0330

    edit

--- Order/forms.py
+++ Order/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Order
+from .models import Order, ShippingMethod
 
 
 class OrderForm(forms.Form):
@@ -27,17 +27,15 @@ class OrderForm(forms.Form):
         }),
     )
 
-    shipping_methods = [
-        ('post', 'پست'),
-        ('tipax', 'تیپاکس'),
-        ('delivery', 'پیک موتوری'),
-    ]
-
-    shipping_method = forms.ChoiceField(
-        widget=forms.RadioSelect,
-        choices=shipping_methods,
+    shipping_method = forms.ModelChoiceField(
+        queryset=ShippingMethod.objects.all(),
+         empty_label="روش ارسال",
+         widget=forms.Select(attrs={
+             'class': 'form-control'
+         }),
     )
 
+    # widget=forms.RadioSelect
     # description = forms.CharField(
     #     label='توضیحات',
     #     required=False,
@@ -82,3 +80,59 @@ class UpdateOrderForm(forms.ModelForm):
             }),
 
         }
+
+
+class filterOrderForm(forms.Form):
+    store_name = forms.CharField(
+        label='نام فروشگاه',
+        required=False,
+        widget=forms.TextInput(attrs=
+        {
+            'class': 'form-control',
+            'placeholder': 'نام فروشگاه',
+            'id': 'store_id_input',
+            'readonly': 'readonly',
+            'type': 'hidden',
+            'rows': 3,
+        }),
+    )
+
+    order_number = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'شماره سفارش',
+            'required': False
+        }))
+
+    from_date = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'از تاریخ',
+        }))
+
+    to_date = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'تا تاریخ',
+            'required': False
+        }))
+
+    shipping_method = forms.ModelChoiceField(
+        required=False,
+        queryset=ShippingMethod.objects.all(),
+        empty_label="روش ارسال",
+        widget=forms.Select(attrs={
+            'class': 'form-control'
+        }),
+    )
+
+    document_defects = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'نقص مدارک',
+            'required': False
+        }))
