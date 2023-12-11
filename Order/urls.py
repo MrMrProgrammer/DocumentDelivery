c97d856bae6c0e5c7584d83120f073d21eeb31eb
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-11 09:35:42 +0330

    add report

--- Order/urls.py
+++ Order/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('show-orders', views.show_order, name='show-orders'),
     path('update_order/<int:order_id>', views.UpdateOrderView.as_view(), name='update_order'),
     path('delete_order/<int:order_id>', views.delete_order, name='delete_order'),
-    path('get_report', views.get_report, name='get_report'),
 
     path('export-to-excel/', views.export_to_excel, name='export_to_excel'),
 
