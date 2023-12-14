4da885992b540698210d14b6ade1aea6b529064e
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-14 08:35:11 +0330

    add filter store in show store page

--- Store/views.py
+++ Store/views.py
@@ -5,15 +5,22 @@ from django.views import View
 from .models import Store
 from .forms import StoreRegisterForm
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
-from .forms import UpdateStoreForm
+from .forms import UpdateStoreForm, FilterStoreForm
+from django.db.models import Q
 
 
 @login_required
 def show_store(request):
-    stores = Store.objects.filter(is_delete=False)
+
+    filter_store = FilterStoreForm(request.POST)
+
+    stores = Store.objects.all()
+    found_stores = Store.objects.filter(is_delete=False)
 
     context = {
         'stores': stores,
+        'found_stores': found_stores,
+        'filterStoreForm': filter_store,
     }
 
     return render(request, 'Store/show-stores.html', context)
@@ -53,6 +60,7 @@ class StoreRegisterView(View):
         # return render(request, 'Store/add-store.html', context)
 
 
+@login_required
 def delete_store(request, store_id):
     store = Store.objects.filter(id=store_id).first()
     store.is_delete = True
@@ -61,6 +69,7 @@ def delete_store(request, store_id):
     return redirect('show-store')
 
 
+@method_decorator(login_required, name='dispatch')
 class UpdateStoreView(View):
 
     def get(self, request: HttpRequest, store_id):
@@ -90,3 +99,28 @@ class UpdateStoreView(View):
         }
 
         return render(request, 'Store/update-store.html', context)
+
+
+@login_required
+def filter_stores(request: HttpRequest):
+    is_filter = True
+
+    filter_store = FilterStoreForm(request.POST)
+    stores = Store.objects.all()
+
+    if filter_store.is_valid():
+        store_name = filter_store.cleaned_data.get('store_name')
+
+        found_stores = Store.objects.filter(
+            Q(is_delete=False),
+            Q(id=store_name) if store_name else Q(),
+        )
+
+        context = {
+            'filterStoreForm': filter_store,
+            'stores': stores,
+            'is_filter': is_filter,
+            'found_stores': found_stores,
+        }
+
+        return render(request, 'Store/show-stores.html', context)
